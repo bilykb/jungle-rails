8675cc0cdcd3689f1e3b43b6cab8cfd2558cccb4
Brett Bilyk <brettbilyk@gmail.com>
2022-01-25 17:05:29 -0700

    Add validation and .downcase for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  before_validation { email.downcase! }
+
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
